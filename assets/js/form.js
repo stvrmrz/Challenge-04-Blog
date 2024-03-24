ec54f3d1d861a04f81c305b124fb6c9f6cf02b61
Steve <stvchrm@steves-mbp.lan>
2024-03-24 01:39:30 -0500

    Build blog html redirect, fix links, and button

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,20 +1,12 @@
-const firstNameInput = document.querySelector('#username');
-const lastNameInput = document.querySelector('#title');
-const emailInput = document.querySelector('#email');
-const passwordInput = document.querySelector('#password');
-const signUpButton = document.querySelector('#sign-up');
 
-signUpButton.addEventListener('click', function (event) {
-  event.preventDefault();
-
-  // create user object from submission
-  const user = {
-    firstName: firstNameInput.value.trim(),
-    lastName: lastNameInput.value.trim(),
-    email: emailInput.value.trim(),
-    password: passwordInput.value.trim(),
-  };
-
-  // set new submission to local storage
-  localStorage.setItem('user', JSON.stringify(user));
-});
+     const form = document.getElementById("blog-form");
+  
+    // Add event listener for form submission
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      // Redirect to posts page
+      window.location.href = "blog.html";
+    });
+    
+  
