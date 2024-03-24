edfab02c0f08958c7c017f86c5cb0aeed45cef40
Steve <stvchrm@steves-mbp.lan>
2024-03-24 02:42:22 -0500

    Get rid of html5 attribute and add form error logic

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,6 +4,15 @@
     // Add event listener for form submission
     form.addEventListener("submit", function(event) {
       event.preventDefault();
+      const username = form.username.value;
+      const title = form.title.value;
+      const contentText = form.content.value;
+      const formError = document.getElementById("form-error");
+
+      if (username === '' || title === '' || contentText === '') {
+        formError.textContent = "Please complete all fields before submitting.";
+        return;
+      } 
   
       // Redirect to posts page
       window.location.href = "blog.html";
