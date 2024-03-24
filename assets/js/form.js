9222d808c42bacf941cdb9cecb8b17e55e6a2820
Steve <stvchrm@steves-mbp.lan>
2024-03-24 03:01:33 -0500

    Create form object so I can get/set their data

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,13 +6,20 @@
       event.preventDefault();
       const username = form.username.value;
       const title = form.title.value;
-      const contentText = form.content.value;
+      const content = form.content.value;
       const formError = document.getElementById("form-error");
 
-      if (username === '' || title === '' || contentText === '') {
+      if (username === '' || title === '' || content === '') {
         formError.textContent = "Please complete all fields before submitting.";
         return;
       } 
+
+      // Use localStorage to store blog post data
+      const blogPostData = {
+        username: username,
+        title: title,
+        content: content
+      }
   
       // Redirect to posts page
       window.location.href = "blog.html";
